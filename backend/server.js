0077da8d9db0c1215c52716b65eb24cc47c29fd5
Glaucon <viniciusglaucon@gmail.com>
2025-07-02 11:34:52 -0300

    sua mensagem de commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://plataformagames.onrender.com",
+  "https://plataformagames-nyle.onrender.com",
 ];
 
 app.use(express.json());
