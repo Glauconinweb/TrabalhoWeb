2ceb130f11122b2094ca8523aa3756bf3178710e
Glaucon <viniciusglaucon@gmail.com>
2025-07-25 23:01:43 -0300

    Atualizei bug navegador

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-// src/server.js ou app.js
+// src/server.js
 
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import authRoutes from "./src/routes/authRouter.js";
 import gameRoutes from "./src/routes/gameRoutes.js";
 
@@ -10,6 +13,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -35,10 +41,12 @@ app.use(
 app.use("/auth", authRoutes);
 app.use("/games", gameRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Servidor rodando! ✅");
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log(` Servidor rodando em http://localhost:${PORT}`);
 });
